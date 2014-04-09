1ad9b0a6d3ab2557783517d3a8a1075eaaabdcb4
Richard Pastenes <richard@pastenes.com>
2014-04-09 15:15:07 -0700

    Added first two sections. Parallax.

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Bootstrap 101 Template</title>
+    <title>Richard Pastenes - Web Dev</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,500,600,300,200,100' rel='stylesheet' type='text/css'>
     <link href="_/css/bootstrap.min.css" rel="stylesheet">
-    <link href="_/css/mystyle.css" rel="stylesheet">
+    <link href="_/css/mystyles.css" rel="stylesheet">
     <script src="_/js/respond.js"></script>
   </head>
   <body>
+  <!-- section 1 - nav -->
   <div class="content row">
     <div class="col-lg-12">
       <header class="clearfix">
@@ -31,7 +33,43 @@
     </div> <!-- column -->
   </div> <!-- content -->
 
+  <!-- section 2 - intro -->
+  <section id="intro" data-speed="6" data-type="background">
+    <div class="container">
+      <div class="row-fluid">
+            <h1>Hello</h1>
+        </div>
+      </div>
+  </section>
+
+  <!-- section 2 - home -->
+  <section id="home">
+    <div class="container">
+      <div class="row-fluid">
+        <div class="col-xs-6 col-md-4">
+          <a href="#item1" class="thumbnail">
+            <img src="images/screens/weis_screenshot_400x211-01.png" alt="Health insurance web site">
+          </a>
+        </div>
+
+        <div class="col-xs-6 col-md-4">
+          <a href="#item1" class="thumbnail">
+            <img src="images/screens/gresham_screenshot_400x211-01.png" alt="Custom WordPress Theme">
+          </a>
+        </div>
+
+        <div class="col-xs-6 col-md-4">
+          <a href="#item1" class="thumbnail">
+            <img src="images/screens/pastenes_screenshot_400x211-01.png" alt="This site's description">
+          </a>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
     <script src="http://code.jquery.com/jquery-latest.min.js"></script>
     <script src="_/js/bootstrap.min.js"></script>
+    <script src="_/js/myscript.js"></script>
   </body>
 </html>
